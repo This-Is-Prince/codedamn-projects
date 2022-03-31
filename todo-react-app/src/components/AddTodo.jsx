cc577f52afad83f55948d245327ea17336b2083c
Prince <this.is.kumar.prince@gmail.com>
2022-03-31 17:28:44 +0530

    add-todo almost completed

--- todo-react-app/src/components/AddTodo.jsx
+++ todo-react-app/src/components/AddTodo.jsx
@@ -1,5 +1,17 @@
+import { FaPlus, FaTrashAlt } from "react-icons/fa";
+
 const AddTodo = () => {
-  return <section className="add-todo"></section>;
+  return (
+    <section className="add-todo">
+      <input type="text" className="add-todo-input" placeholder="Add Task..." />
+      <button className="btn add-btn center">
+        <FaPlus></FaPlus>
+      </button>
+      <button className="btn clear-btn center">
+        <FaTrashAlt></FaTrashAlt>
+      </button>
+    </section>
+  );
 };
 
 export default AddTodo;
--- todo-react-app/src/components/Header.jsx
+++ todo-react-app/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header center">
       <h1>add todo list</h1>
       <span className="underline"></span>
     </header>
